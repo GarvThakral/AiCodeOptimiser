de2343ba76465a04906929155f8ab6675425d005
Garv Thakral <garvthakralfx@gmail.com>
2025-03-17 09:57:15 +0530

    working ver

--- index.py
+++ index.py
@@ -6,12 +6,52 @@ from api_call import optimize_code
 from pygments.lexers import guess_lexer    
 from tkinter import ttk
 from tkcode import CodeEditor
+from tkcode import codebox
+import sqlite3
+con = sqlite3.connect("tutorial.db")
+
+LANGUAGE_MAP = {
+    "Python": "python",
+    "C++": "cpp",
+    "JavaScript": "javascript",
+    "HTML": "html",
+    "CSS": "css",
+    "Java": "java",
+    "Ruby": "ruby",
+    "Go": "go",
+    "Rust": "rust",
+    "PHP": "php",
+    "SQL": "sql",
+    "Markdown": "markdown",
+    "Shell": "shell",
+    "TypeScript": "typescript",
+    "Scala": "scala",
+    "Lua": "lua",
+    "Swift": "swift",
+    "Objective-C": "objective-c",
+    "Kotlin": "kotlin",
+    "Perl": "perl",
+    "Dart": "dart",
+    "R": "r",
+    "Haskell": "haskell",
+    "VHDL": "vhdl",
+    "Pascal": "pascal",
+    "Elixir": "elixir",
+    "Julia": "julia",
+    "ShellSession": "shell",
+    "Plain Text": "text",
+    "Assembly": "asm",
+    "Objective-C++": "cpp",
+    "TeX": "tex",
+    "YAML": "yaml",
+    "Tcl": "tcl",
+}
 
 class AICodeOptimizer:
     def __init__(self):
         self.root = tk.Tk()
         self.root.geometry("1400x900")
-        self.language = ""
+        self.language = "Python"
         self.root.columnconfigure(0, weight=1)  
         self.root.columnconfigure(1, weight=1)  
         self.root.columnconfigure(2, weight=1)  
@@ -25,13 +65,13 @@ class AICodeOptimizer:
         self.languageLabel.grid(row=0, column=2, pady=10, sticky="n")
 
 
-        self.code_editor = CodeEditor(self.root, language={self.language}, width = 50)
+        self.code_editor = CodeEditor(self.root, language=self.language, width = 50)
         self.code_editor.grid(row=1, column=0, pady=10, sticky="nsew")
         self.code_editor.bind("<<Modified>>",self.detectLanguage)
         self.code_editor.bind("<KeyPress>",self.detectLanguage)
         self.code_editor.bind("<KeyRelease>",self.detectLanguage)
 
-        self.code_editor1 = CodeEditor(self.root, language={self.language}, width = 50)
+        self.code_editor1 = CodeEditor(self.root, language=self.language, width = 50)
         self.code_editor1.grid(row=1, column=2, pady=10, sticky="nsew")
 
         self.buttonOptimise = tk.Button(self.root, text="Optimise Code", command=lambda: self.callOptimise(self.code_editor.get("1.0", tk.END)))
@@ -51,12 +91,20 @@ class AICodeOptimizer:
         # self.aiTextLabel = tk.Label(self.root, text="")
         # self.aiTextLabel.grid(row=5, column=1, pady=10, rowspan=3)
 
-    def detectLanguage(self,event):
-        print('Triggererd')
+    def detectLanguage(self, event):
+        print('Triggered')
         code = self.code_editor.get("1.0", "end-1c")
         lexer = guess_lexer(code)
-        self.language = lexer.name
-        self.languageLabel.config(text = "Language : "+self.language)    
+        detected_language = lexer.name
+
+        mapped_language = LANGUAGE_MAP.get(detected_language, "text") 
+
+        self.language = mapped_language
+        self.languageLabel.config(text = "Language : " + detected_language)
+
+        self.language = mapped_language
+        self.language = mapped_language
+ 
         
     def callOptimise(self, code_snippet):
         if(len(code_snippet) == 1 or code_snippet == "The code cannot be empty"):
